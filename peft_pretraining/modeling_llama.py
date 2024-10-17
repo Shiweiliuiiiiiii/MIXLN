f43c54598b61d676f0b4935dc9933b7efa8c20f2
Shiweiliuiiiiiii <s.liu3@tue.nl>
2024-10-17 11:56:36 +0100

    4 postLN layers

--- peft_pretraining/modeling_llama.py
+++ peft_pretraining/modeling_llama.py
@@ -269,7 +269,7 @@ class LlamaDecoderLayer(nn.Module):
         
         scale_attn_weights = False
         scale_mlp_output = False
-        self.max_post_norm_layer = 6
+        self.max_post_norm_layer = 4
 
         if norm_type == 'scale_post_pre':
             if self.layer_index < self.max_post_norm_layer:
