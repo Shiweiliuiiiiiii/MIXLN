3f06c78262b07dda8d8165c80f52a0a45cd63a32
Shiweiliuiiiiiii <s.liu3@tue.nl>
2024-10-29 17:58:38 +0000

    1b long

--- run_1b.sh
+++ run_1b.sh
@@ -8,17 +8,19 @@ master_port=$4
 
 echo "Training with learning rate: $learning_rates, norm type: $norm_type on GPU $gpu"
 
-CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=$master_port torchrun_main.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node 4 --master_port=$master_port torchrun_main.py \
     --model_config configs/llama_1b.json \
     --lr $learning_rates \
     --batch_size 64 \
     --total_batch_size 512 \
-    --num_training_steps 50000 \
-    --warmup_steps 5000 \
+    --num_training_steps 100000 \
+    --warmup_steps 10000 \
     --weight_decay 0 \
     --dtype bfloat16 \
     --eval_every 1000 \
     --optimizer adam \
     --grad_clipping 0.0 \
     --run_name "1b_res_${norm_type}_lr${learning_rates}" \
-    --save_dir "/scratch/shiwei/models/1b_${norm_type}_${POST_NUM}_lr${learning_rates}"
+    --save_dir "/scratch/shiwei/models/1b_11B_${norm_type}_${POST_NUM}_lr${learning_rates}"
+
+
